e1686fe1ddcf76d4ca55b3cfd83c562d1aa9f33c
ChloeArd <chloe.ardoise@gmail.com>
2021-01-12 13:58:31 +0100

    Exercice 122

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $(".hole");
+const scoreBoard = $(".score");
+let score = 0;
+const moles = $(".mole");
+
+// mettre 10s
+$("#startGame").click(function (){
+    startGame();
+})
+
+let trouHasard = holes[Math.floor(Math.random() * holes.length)];
+let intervalTempsHasard = Math.random() * (1000 - 200) + 200;
+function startGame() {
+    $(trouHasard).addClass("up");
+    moles.show(speed(intervalTempsHasard));
+}
+
+// Lorqu'on clique sur une taupe le score augmente de 1 a chaque fois
+moles.click(function () {
+    $(this).each(function (){
+        trouHasard = holes[Math.floor(Math.random() * holes.length)];
+        score++;
+        scoreBoard.html(score);
+        setInterval(function () {
+            startGame();
+        },intervalTempsHasard);
+        $(trouHasard).removeClass("up");
+    })
+})
+
+
+

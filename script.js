60faffd55bf3b0387c6f90703eb2d334d89e7803
ChloeArd <chloe.ardoise@gmail.com>
2021-01-14 13:50:58 +0100

    Exercice 122

--- script.js
+++ script.js
@@ -2,31 +2,35 @@ const holes = $(".hole");
 const scoreBoard = $(".score");
 let score = 0;
 const moles = $(".mole");
-
-// mettre 10s
-$("#startGame").click(function (){
-        startGame();
-})
-
 let trouHasard = holes[Math.floor(Math.random() * holes.length)];
 let intervalTempsHasard = Math.random() * (1000 - 200) + 200;
+
+$("#startGame").click(function (){
+        startGame();
+
+})
+
 function startGame() {
     $(trouHasard).addClass("up");
-    moles.show(speed(intervalTempsHasard));
+    moles.click(function () {
+        $(this).each(function (){
+            $(trouHasard).removeClass("up");
+            trouHasard = holes[Math.floor(Math.random() * holes.length)];
+            score++;
+            scoreBoard.html(score);
+            setInterval(function () {
+                $(trouHasard).addClass("up");
+            },intervalTempsHasard);
+        })
+    })
+    setTimeout(function () {
+        alert("FINI, ton score final est de " + score + "!!!");
+        location.reload();
+    }, 10000)
 }
 
 // Lorqu'on clique sur une taupe le score augmente de 1 a chaque fois
-moles.click(function () {
-    $(this).each(function (){
-        $(trouHasard).removeClass("up");
-        trouHasard = holes[Math.floor(Math.random() * holes.length)];
-        score++;
-        scoreBoard.html(score);
-        setInterval(function () {
-            startGame();
-        },intervalTempsHasard);
-    })
-})
+
 
 
 
